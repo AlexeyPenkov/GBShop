43bfd66f71330aaf9f83a8d1e1d89137cbeea1bd
Алексей Пеньков <alexey_penkov@list.ru>
2022-01-14 17:19:21 +0800

    где-то свернул не туда
    перенес код из методички. повываливались ошибки. Что то мне от абстракщины уже тошно

---
+++ GBShopTests/PostStub.swift
@@ -0,0 +1,15 @@
+//
+//  PostStub.swift
+//  GBShopTests
+//
+//  Created by Алексей Пеньков on 14.01.2022.
+//
+
+import Foundation
+
+struct PostStub: Codable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
